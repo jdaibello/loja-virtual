009680de5821e354066508ab37a384bcc13a870e
João Pedro <jpedrodaibello1999@outlook.com>
2020-07-03 13:19:29 -0300

    Implementando navegação via Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,21 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/base/base_screen.dart';
 
 void main() {
   runApp(MyApp());
-  
-  Firestore.instance.collection('teste').add({'teste': 'teste'});
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Loja do João',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(),
+      home: BaseScreen(),
     );
   }
 }
