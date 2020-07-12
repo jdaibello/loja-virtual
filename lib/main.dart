a7731e955eb4df133ee89cf596f7cdd8f804524c
João Pedro <jpedrodaibello1999@outlook.com>
2020-07-11 21:39:30 -0300

    Busca dos Produtos

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/product.dart';
+import 'package:loja_virtual/models/product_manager.dart';
 import 'package:loja_virtual/models/user_manager.dart';
 import 'package:loja_virtual/screens/base/base_screen.dart';
 import 'package:loja_virtual/screens/login/login_screen.dart';
+import 'package:loja_virtual/screens/product/product_screen.dart';
 import 'package:loja_virtual/screens/signup/signup_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -14,36 +17,49 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => UserManager(),
-      lazy: false,
-      child: MaterialApp(
-        title: 'Loja do João',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: const Color.fromARGB(255, 4, 125, 141),
-          scaffoldBackgroundColor: const Color.fromARGB(255, 4, 125, 141),
-          appBarTheme: const AppBarTheme(
-            elevation: 0
-          ),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserManager(),
+          lazy: false,
         ),
-        initialRoute: '/base',
-        onGenerateRoute: (settings) {
-          switch(settings.name) {
-            case '/login':
-              return MaterialPageRoute(
-                  builder: (_) => LoginScreen()
-              );
-            case '/signup':
-              return MaterialPageRoute(
-                builder: (_) => SignUpScreen()
-              );
-            case '/base':
-            default:
-              return MaterialPageRoute(
-                  builder: (_) => BaseScreen()
-              );
+        ChangeNotifierProvider(
+          create: (_) => ProductManager(),
+          lazy: false,
+        ),
+      ], child: MaterialApp(
+      title: 'Loja do João',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color.fromARGB(255, 4, 125, 141),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 4, 125, 141),
+        appBarTheme: const AppBarTheme(
+          elevation: 0
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/base',
+      onGenerateRoute: (settings) {
+        switch(settings.name) {
+          case '/login':
+            return MaterialPageRoute(
+                builder: (_) => LoginScreen()
+            );
+          case '/signup':
+            return MaterialPageRoute(
+              builder: (_) => SignUpScreen()
+            );
+          case '/product':
+            return MaterialPageRoute(
+                builder: (_) => ProductScreen(
+                  settings.arguments as Product
+                )
+            );
+          case '/base':
+          default:
+            return MaterialPageRoute(
+                builder: (_) => BaseScreen()
+            );
           }
         },
       ),
